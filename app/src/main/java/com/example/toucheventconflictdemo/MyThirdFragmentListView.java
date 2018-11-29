1fe4c7dabf69c9631701ec590f641703f9990e1d
wangjiangtao <tongchexinfeitao@sina.cn>
2018-11-29 15:56:07 +0800

    add message

--- app/src/main/java/com/example/toucheventconflictdemo/MyThirdFragmentListView.java
+++ app/src/main/java/com/example/toucheventconflictdemo/MyThirdFragmentListView.java
@@ -27,21 +27,26 @@ public class MyThirdFragmentListView extends ListView {
         switch (action) {
             case MotionEvent.ACTION_DOWN:
                 super.onInterceptTouchEvent(ev);
+                //不允许上层viewGroup拦截事件
                 getParent().requestDisallowInterceptTouchEvent(true);
                 break;
             case MotionEvent.ACTION_MOVE:
                 //满足listView滑动到顶部，如果继续下滑，那就让scrollView拦截事件
                 if (getFirstVisiblePosition() == 0 && (ev.getY() - mLastY) > 0) {
+                    //允许上层viewGroup拦截事件
                     getParent().requestDisallowInterceptTouchEvent(false);
                 }
                 //满足listView滑动到底部，如果继续上滑，那就让scrollView拦截事件
                 else if (getLastVisiblePosition() == getCount() - 1 && (ev.getY() - mLastY) < 0) {
+                    //允许上层viewGroup拦截事件
                     getParent().requestDisallowInterceptTouchEvent(false);
                 } else {
+                    //不允许上层viewGroup拦截事件
                     getParent().requestDisallowInterceptTouchEvent(true);
                 }
                 break;
             case MotionEvent.ACTION_UP:
+                //不允许上层viewGroup拦截事件
                 getParent().requestDisallowInterceptTouchEvent(true);
                 break;
         }
